d3224488893dc02e15913a275b89c2bbbe13123b
Morgan Elliot <m.33.elliot@gmial.com>
2022-03-15 17:25:16 +0800

    Created database apdated prop ref paths which rely on database

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,10 +4,10 @@ export default function Card(props) {
     return (
         <div className="card">
             <div className="card--img-cont">
-                <img className="card--img" src={pin} />
+                <img className="card--img" src={props.image} />
             </div>
             <div className="card--info">
-                <img className="card--pin" />
+                <img className="card--pin" src={pin}/>
                 <span>{props.country}</span>
                 <span className="grey underline">View on Google Maps</span>
                 <h1 className="bold">{props.title}</h1>
