4970108488c9d5ae9bfa39c0b9f5c4dcb637f394
Morgan Elliot <m.33.elliot@gmial.com>
2022-03-15 16:12:58 +0800

    Additional files that didn't make it into last commit

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import pin from "../images/geo-pin.png";
+
+export default function Card(props) {
+    return (
+        <div className="card">
+            <div className="card--img-cont">
+                <img className="card--img" src={pin} />
+            </div>
+            <div className="card--info">
+                <img className="card--pin" />
+                <span>{props.country}</span>
+                <span className="grey underline">View on Google Maps</span>
+                <h1 className="bold">{props.title}</h1>
+                <span className="bold">{props.startDate}</span>
+                <p>{props.discription}</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import globe from "../images/globe-logo.png";
+
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <img className="nav--logo" src={globe} />
+            <h1 className="nav--title">My Travel Journal</h1>
+        </nav>
+    )
+}
